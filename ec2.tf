cd8495a706c7a185a181e8b36ae998c66be49a0e
Vanessa <vanessa.nunez.p@gmail.com>
2024-02-22 22:45:29 -0300

    change job name

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "amb-prod" {
-    ami           = "ami-0c7217cdde317cfec"
+  ami           = "ami-0c7217cdde317cfec"
   instance_type = "t2.micro"
   key_name = "terraform-rsa-pem"
   security_groups = ["allow_ssh", "allow_http", "allow_egress"]
